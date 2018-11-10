d670ea94d02b90d681da7a1bcdbe6e23fbd75cb1
Hy Huynh <hyxhuynh@gmail.com>
2018-11-10 03:14:02 -0800

    initial commit

---
+++ app/data/friends.js
@@ -0,0 +1,37 @@
+var friendsArray = [
+    {
+        "name":"Shizuka",
+        "photo": "https://cdn.pixilart.com/photos/large/03295c8d8e07924.jpg",
+        "scores":[
+            5,
+            1,
+            4,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            1
+        ]
+    },
+    {
+        "name":"Baymax",
+        "photo":"https://www.yourwdwstore.net/assets/images/6/60000/6000/300/66335e.jpg",
+        "scores":[
+            4,
+            3,
+            4,
+            2,
+            5,
+            3,
+            2,
+            1,
+            3,
+            5
+        ]
+    }
+  ];
+  
+  // Note how we export the array. This makes it accessible to other files using require.
+  module.exports = friendsArray;
